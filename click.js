d4144aa9989600e28612545cb45f8faadb2564c2
Feelsofly <39503879+Feelsofly@users.noreply.github.com>
2020-02-18 18:00:29 -0500

    completed

--- click.js
+++ click.js
@@ -1,4 +1,6 @@
+let shadow = document.getElementById('shadow')
 let link = document.getElementById("ball").addEventListener('click',
     function (e) {
         e.target.classList.add('clicked');
+        shadow.classList.add('clicked-shadow');
     })
